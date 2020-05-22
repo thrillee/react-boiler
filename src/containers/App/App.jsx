537e25ccf1e3ae26d71b373773ab8824b469a96a
Oluwatobi Bello <bellotobiloba01@gmail.com>
2020-05-22 21:38:38 +0100

    initial commit

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+  render() {
+    return (
+      <center style={{ margin: 250, fontSize: 50 }}>We Bless Jesus! </center>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ src/containers/App/Router/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+const Router = () => (
+  <main>
+    <Switch></Switch>
+  </main>
+);
+
+export default Router;
